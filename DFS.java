5ab067051f7660c0d6fdb7920dea8593153754fd
Danilo Gonzalez <gonzalezantonio.danilo@gmail.com>
2017-05-04 23:49:33 -0700

    Created initial classes. Tired right now, so will continue tomorrow.

---
+++ DFS.java
@@ -0,0 +1,6 @@
+public class DFS{
+	public static void main(String[] args){
+		Edge e = new Edge(1, 2);
+		System.out.println(e);
+	}
+}
---
+++ Graph.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph{
+	private Map<Integer, Edge> adjacencyList;
+
+	public Graph(){
+		adjacencyList = new HashMap<>();
+	}
+
+	public boolean addEdge(){
+
+	}
+
+	public boolean addVertex(){
+
+	}
+
+	public boolean removeEdge(){
+
+	}
+
+	public boolean removeVertex(){
+
+	}
+
+}
